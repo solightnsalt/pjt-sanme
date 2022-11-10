f55f01d0336098b55c3bab7d7fe27b658b6dd993
woocheolll <woocheol2868@gmail.com>
2022-11-10 10:40:54 +0900

    feat/articles/create생성

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.decorators.http import require_safe
+from .forms import PostForm
+from .models import Post
 
 
 # Create your views here.
@@ -8,3 +10,16 @@ def index(request):
     return render(request, "articles/index.html")
 
 
+def create(request):
+    if request.method == 'POST':
+        post_form = PostForm(request.POST)
+        if post_form.is_valid():
+            posts = post_form.save(commit=False)
+            posts.user = request.user
+            posts.save()
+            return redirect("articles:index")
+    else :
+        post_form = PostForm()
+        
+    
+    return render(request, "articles/create.html",{"post_form" : post_form})
