f9eb425abe3079e1e51fcb5f58009535bcc1c631
solightnsalt <soldakm@gmail.com>
2022-11-10 12:16:00 +0900

    feat/account: login,logout코드 작성

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render, redirect
 from django.views.decorators.http import require_safe
 from django.contrib.auth import login as auth_login
 from .forms import CustomUserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import get_user_model
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 
 # Create your views here.
 @require_safe
@@ -22,3 +29,26 @@ def signup(request):
         "form": form,
     }
     return render(request, "accounts/signup.html", context)
+
+
+def login(request):
+    if request.user.is_annoymous:
+        if request.method == "POST":
+            login_form = AuthenticationForm(request, data=request.POST)
+            if login_form.is_valid():
+                auth_login(request, login_form.get_user())
+                return redirect(request.GET.get("next") or "articles:index")
+        else:
+            login_form.AuthenticationForm()
+
+        context = {
+            "login_form": login_form,
+        }
+        return render(request, "accounts/login.html", context)
+    else:
+        return redirect("accounts:index")
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect("accounts:index")
