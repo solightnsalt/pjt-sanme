f30f1b73de257de3fbbc90a27b62331d96d64cea
JeongJinGan <rksaudwls123@naver.com>
2022-11-10 11:09:45 +0900

    feat/accounts: 모델생성

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.decorators.http import require_safe
-
+from django.contrib.auth import login as auth_login
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 @require_safe
 def index(request):
     return render(request, "accounts/index.html")
+
+
+def signup(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()  # ModelForm의 save메소드의 리턴값은 해당 모델의 인스턴스다
+            auth_login(request, user)  # 로그인
+            return redirect("accounts:index")
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/signup.html", context)
