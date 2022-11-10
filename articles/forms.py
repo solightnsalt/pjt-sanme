f9cf749bb7bec4682ed91bc2324e70c0dbe0b09c
woocheolll <woocheol2868@gmail.com>
2022-11-10 10:15:31 +0900

    feat/forms작성

--- articles/forms.py
+++ articles/forms.py
@@ -1,1 +1,13 @@
 from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = [
+            "title",
+            "day_time",
+            "park_address",
+            "pet",
+            "content",
+        ]
