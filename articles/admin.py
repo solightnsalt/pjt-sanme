2cbbbb5f028522c3d92f73419a402672efae3db9
jejoonlee <jejoonlee1996@gmail.com>
2022-11-10 14:25:09 +0900

    FEAT: articles/admin.py에 정보 넣기

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post, Comment
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
