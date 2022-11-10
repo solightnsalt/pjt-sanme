57d49a70929d1da4eeb2bc890a84423bfff6ffd4
jejoonlee <jejoonlee1996@gmail.com>
2022-11-10 13:46:13 +0900

    FEAT: detail에 comment_form 넣기

--- articles/urls.py
+++ articles/urls.py
@@ -7,9 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("create/", views.create, name="create"),
     path("<int:pk>/detail/",views.detail,name="detail"),
-
     path("<int:pk>/update/",views.update,name="update"),
-
-
     path("<int:pk>/comment/", views.comment, name="comment"),
 ]
