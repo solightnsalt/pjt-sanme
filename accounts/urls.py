f5a2636fac21479d72f4dc8b4c651fec53148697
최준혁 <fx887722@naver.com>
2022-11-10 17:05:24 +0900

    feat/accounts: 유저 상세정보 수정하기

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.login, name="login"),
     path("signup/", views.signup, name="signup"),
     path("detail/<int:pk>/", views.detail, name="detail"),
+    path("update/<int:pk>/", views.update, name="update"),
     path("logout/", views.logout, name="logout"),
 ]
